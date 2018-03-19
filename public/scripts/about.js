9d188ca858b9e03984fc87c3c63dc7fd5b3cd058
Alexandra Briceag <Computer1@Alexandras-MacBook-Pro-2.local>
2018-03-19 22:39:40 +0100

    about iteration

--- public/scripts/about.js
+++ public/scripts/about.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
+	$.get('/json/about.json', function(data){
+
+		var edu = $('<ul></ul>')
+
+		data[1].education.forEach(function(item){
+
+			$(edu).append(`
+				<ul>
+					<li>${item}</li>
+				</ul>`)
+
+				$('.edu').append(edu)
+		})
+
+	})
+
 	
 
 })
